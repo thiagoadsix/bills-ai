109a9e978f36dbefb863c2d8eae725c8dbc78972
Thiago Andrade Silva <thiago.adsix@gmail.com>
2024-09-11 03:48:56 +0000

    Sep 11, 2024, 12:48 AM

--- __local__/resources/aws/dynamodb/create-bills.sh
+++ __local__/resources/aws/dynamodb/create-bills.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-# Define the endpoint URL to LocalStack
 ENDPOINT_URL="http://localhost:4566"
 
-# Create the DynamoDB table
 aws dynamodb create-table \
     --table-name Bills \
     --attribute-definitions AttributeName=id,AttributeType=S \
@@ -11,5 +9,4 @@ aws dynamodb create-table \
     --billing-mode PAY_PER_REQUEST \
     --endpoint-url $ENDPOINT_URL \
     --region us-east-1 \
-    --output json \
-    --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5
+    --output json
